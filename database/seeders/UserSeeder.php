71fc1f82b916621dea456a56518f28e417188cfd
JonatasTB <101901086+jonatastb@users.noreply.github.com>
2025-02-06 23:50:14 -0300

    ajuste no array de todos os rankings

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,9 +13,9 @@ class UserSeeder extends Seeder
     {
 
         $users = [
-            ['name' => 'Joao', 'email' => 'joao@teste.com', 'password' => bcrypt('123456')],
-            ['name' => 'Jose', 'email' => 'jose@teste.com', 'password' => bcrypt('123456')],
-            ['name' => 'Paulo', 'email' => 'paulo@teste.com', 'password' => bcrypt('123456')],
+            ['name' => 'Joao', 'email' => fake()->email, 'password' => bcrypt('123456')],
+            ['name' => 'Jose', 'email' => fake()->email, 'password' => bcrypt('123456')],
+            ['name' => 'Paulo', 'email' => fake()->email, 'password' => bcrypt('123456')],
             ['name' => 'Admin', 'email' => 'admin@teste.com', 'password' => bcrypt('123456')],
         ];
 
